329274068cedc5caf5445b360794a7f29720370b
piterm11 <piteralke@gmail.com>
2021-12-20 10:50:00 +0100

    added finals and remove magic numbers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,28 +5,38 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    private static final int ONE = 1;
+
+    private static final int ZERO = 0;
+
+    private static final int RANGE = 20;
+
+    private static final int NO_RYMMERS = 3;
+
+    private static final int NO_ELEMENTS = 15;
+
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
-        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+                factory.getFIFORhymer(), factory.getHanoiRhymer() };
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = ONE; i < NO_ELEMENTS; i++)
+            for (int j = ZERO; j < NO_RYMMERS; j++)
                 rhymers[j].countIn(i);
 
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = ONE; i < NO_ELEMENTS; i++)
+            rhymers[NO_RYMMERS].countIn(rn.nextInt(RANGE));
 
-        for (int i = 0; i < rhymers.length; i++) {
+        for (int i = ZERO; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
 
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[NO_RYMMERS]).reportRejected());
 
     }
 
