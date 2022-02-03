acce70c72b349ae4d0a9af46a52f2565065a5901
szatanmilio <jediszatanmilio@gmail.com>
2022-02-03 16:53:47 +0100

    Zadanie 2.2 poprawki

---
+++ src/main/java/edu/kis/vh/nursery/dataStructures/Structrues.java
@@ -0,0 +1,23 @@
+package edu.kis.vh.nursery.dataStructures;
+
+public interface Structrues {
+    void countIn(int in);
+
+    int getTotal();
+
+    boolean callCheck();
+
+    int peekaboo();
+
+    int countOut();
+
+    void push(int i);
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int top();
+
+    int pop();
+}
